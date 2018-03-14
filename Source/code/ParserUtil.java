07ac967cf6bc455a21c1f69e9cffd6095b6cc2a5
LBMF132L <35092274+LBMF132@users.noreply.github.com>
2018-03-14 09:17:57 +0300

    Changes by me

---
+++ Source/code/ParserUtil.java
@@ -0,0 +1,39 @@
+package code;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ParserUtil {
+	public static void main(String[] args) {
+
+		// The name of the file to open.
+		String fileName =  "\\Source\\data\\Maps\\Tiles.tsx";
+		// This will reference one line at a time
+		String line = null;
+
+		try {
+			// FileReader reads text files in the default encoding.
+			FileReader fileReader = new FileReader(fileName);
+
+			// Always wrap FileReader in BufferedReader.
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+			while ((line = bufferedReader.readLine()) != null) {
+				System.out.println(line);
+			}
+
+			// Always close files.
+			bufferedReader.close();
+		} catch (FileNotFoundException ex) {
+			System.out.println("Unable to open file '" + fileName + "'");
+		} catch (IOException ex) {
+			System.out.println("Error reading file '" + fileName + "'");
+			// Or we could just do this:
+			// ex.printStackTrace();
+		}
+	}
+
+}
