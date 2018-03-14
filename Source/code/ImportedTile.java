d8e3715c807b33b41400119cbfb6162161eaa339
LBMF132L <35092274+LBMF132@users.noreply.github.com>
2018-03-14 17:56:34 +0300

    Parse commit

--- Source/code/ImportedTile.java
+++ Source/code/ImportedTile.java
@@ -2,12 +2,12 @@ import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 
-public class Image {
+public class ImportedTile {
 	public String path;
 	public int id;
 	public int x;
 	public int y;
-	public Image(String p, int i, int xd, int yd) {
+	public ImportedTile(String p, int i, int xd, int yd) {
 		path=p;
 		id=i;
 		x=xd;
--- Source/code/ParserUtil.java
+++ Source/code/ParserUtil.java
@@ -8,11 +8,55 @@ import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 
 public class ParserUtil {
 	public static void main(String[] args) {
 
 		String fileName =  "C:\\Users\\19lfreeman\\Documents\\GitHub\\PlatformerGame\\Source\\code\\Tiles.tsx";
+		// This will reference one line at a time
+		String line = null;
+
+		readTileSet(fileName);
+	}
+	public static Scene readCSV(String fPath) {
+		Pane pane = new Pane();
+		Scene scene = new Scene(pane,1000,1000);
+		try {
+			FileReader fileReader = new FileReader(fPath);
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			String line=null;
+			ArrayList<String> allStrings = new ArrayList<String>(5);
+			while ((line = bufferedReader.readLine()) != null) {
+				allStrings.add(line);
+			}
+			int mapWidth = allStrings.get(0).split(",").length;
+			int mapHeight = allStrings.size();
+			Block[][] mapBlocks = new Block[mapHeight][mapWidth];
+			for (int i = 0; i < allStrings.size(); i++) {
+				String sRow = allStrings.get(i);
+				//top row is first
+				String[] splitRow = sRow.split(",");
+				for(int j=0;j<splitRow.length;j++) {
+					String num = splitRow[j];
+					int bID = Integer.parseInt(num);
+					
+				}
+			}
+		}
+		catch (FileNotFoundException ex) {
+			System.out.println("Unable to open file '" + fPath + "'");
+		} catch (IOException ex) {
+			System.out.println("Error reading file '" + fPath + "'");
+		}
+		return scene;
+	}
+	public static ArrayList<ImportedTile> readTileSet(String fpath){
+		ArrayList<ImportedTile> tiles = new ArrayList<ImportedTile>();
+		String fileName = fpath;
 		// This will reference one line at a time
 		String line = null;
 
@@ -35,9 +79,12 @@ public class ParserUtil {
 					if(line.indexOf("id=")!=-1) {
 						String nLine = bufferedReader.readLine();
 						String path = nLine.substring(nLine.indexOf("source")+7, nLine.length()-2);
+//						line=bufferedReader.readLine();
 						int id = Integer.parseInt(line.substring(11,line.indexOf("\">")));
-						System.out.println(id);
-
+						int x = Integer.parseInt(nLine.substring(16,nLine.indexOf("\" h")));
+						int y = Integer.parseInt(nLine.substring(nLine.indexOf("t=")+3,nLine.indexOf("\" s")));
+						ImportedTile iTile = new ImportedTile(path,id,x,y);
+						tiles.add(iTile);
 					}
 				}
 			}
@@ -51,6 +98,7 @@ public class ParserUtil {
 			// Or we could just do this:
 			// ex.printStackTrace();
 		}
+		return tiles;
 	}
-
+	
 }
